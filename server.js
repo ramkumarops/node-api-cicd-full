9fdbb6dcd2fb2e8e8e1443bd57c74dfdf63b0d0f
ramkumarops <70690240+ramkumarops@users.noreply.github.com>
2025-11-13 20:09:00 +0530

    update

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+app.get('/', (req, res) => {
+
+    res.send('Hello World');
+});
+
+export default app;
